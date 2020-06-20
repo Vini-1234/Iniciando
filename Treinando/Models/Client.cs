c88144868d612611ad94ec8b383dd720a68e70d7
Vinicius Suriano <vinicius_suriano@hotmail.com>
2020-06-20 00:05:47 -0300

    Criacao do client e company

---
+++ Treinando/Models/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Treinando.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public ICollection<Company> Companies { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+    }
+}
